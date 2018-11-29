a18a225f57fbdbbfc91de7621910bf452036d191
Gnidleif <gnidleif@gmail.com>
2018-11-29 10:04:26 +0100

    minor text change

--- 2017/C#/Program.cs
+++ 2017/C#/Program.cs
@@ -6,6 +6,8 @@ namespace C_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(new Day01("1122").Part1());
+            Console.WriteLine(new Day01("1212").Part2());
         }
     }
 }
