6caf9316d6f5c8fd5874dc3b609a54ef4623f61f
Gnidleif <gnidleif@gmail.com>
2018-12-05 11:35:41 +0100

    Update Day05.lua

--- 2018/Lua/Day05.lua
+++ 2018/Lua/Day05.lua
@@ -1,26 +1,17 @@
-function part1(line, start)
-    for i = start, #line-1 do
-        local r = string.byte(line:sub(i,i)) - string.byte(line:sub(i+1,i+1))
-        if r == 32 or r == -32 then
-            local tmp = string.format("%s%s", line:sub(1,i-1), line:sub(i+2))
-            local idx = i-1
-            if i == 1 then
-                idx = 1
-            end
-            return part1(tmp, idx)
-        end
-    end
-    return #line
+function part1(line)
+    return reduce(line, 1)
 end
 
 function part2(line)
-    local abet = "abcdefghijklmnopqrstuvwxyz"
+    local abet = get_alphabet()
+    -- the length of the original line is as long as it's going to get
     local lowest = #line
-    for i = 1, #abet do
-        local sub = remove_char(line, abet:sub(i,i))
+    for _, char in pairs(abet) do
+        local sub = remove_char(line, char)
+        -- if nothing was removed, there's no need to reduce
         if #sub ~= #line then
-            local num = part1(sub, 1)
-            if num < lowest then
+            local num = part1(sub)
+            if num < #sub and num < lowest then
                 lowest = num
             end
         end
@@ -28,17 +19,49 @@ function part2(line)
     return lowest
 end
 
+function reduce(line, start)
+    for i = start, #line-1 do
+        -- string.byte("A") == 65, string.byte("a") == 97
+        -- 97-65 == 32, 65-97 == -32
+        local slice = line:sub(i,i+1)
+        local res = slice:sub(1,1):byte() - slice:sub(2,2):byte()
+        if res == 32 or res == -32 then
+            -- splices the string to remove the reductionable pair
+            -- note: if i-1 == 0 the function still works
+            local tmp = string.format("%s%s", line:sub(1,i-1), line:sub(i+2))
+            local idx = i-1
+            if i == 1 then
+                idx = 1
+            end
+            return reduce(tmp, idx)
+        end
+    end
+    return #line
+end
+
 function remove_char(line, char)
+    -- pattern should be ['CHAR''char'] to find any occurence of the char
     local pattern = string.format("[%s%s]", string.upper(char), string.lower(char))
+    -- if pattern doesn't match, just return the full string again
     if string.find(line, pattern) == nil then
         return line
     end
     return line:gsub(pattern, '')
 end
 
+function main()
+    local line = read_file("Day05.txt")[1]
+    --line = "dabAcCaCBAcCcaDA"
+    print(part1(line))
+    print(part2(line))
+end
+
+-- HELPER FUNCTIONS
 function file_exists(file)
     local f = io.open(file, "rb")
-    if f then f:close() end
+    if f then 
+        f:close()
+    end
     return f ~= nil
 end
 
@@ -51,6 +74,12 @@ function read_file(file)
     return lines
 end
 
-local line = read_file("Day05.txt")[1]
-print(part1(line, 1))
-print(part2(line))
+function get_alphabet()
+    local letters = {}
+    for ascii = 97, 122 do
+        table.insert(letters, string.char(ascii))
+    end
+    return letters
+end
+
+main()
